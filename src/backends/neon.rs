f920a6e117b69230dfb40b86733498413d930bcd
silverstillisntgold <nickdilla13@gmail.com>
2025-03-23 02:05:52 -0400

    fixing neon backend attempt #1

--- src/backends/neon.rs
+++ src/backends/neon.rs
@@ -2,20 +2,27 @@ use crate::util::*;
 use core::arch::aarch64::*;
 use core::{mem::transmute, ops::Add};
 
+#[derive(Clone, Copy)]
+union InternalRow {
+    u32x4: uint32x4_t,
+    u64x2: uint64x2_t,
+}
+
 #[derive(Clone)]
 pub struct Matrix {
-    state: [[uint32x4_t; WIDTH]; DEPTH],
+    state: [[InternalRow; WIDTH]; DEPTH],
 }
 
 impl Add for Matrix {
     type Output = Self;
 
-    #[inline(always)]
+    #[inline]
     fn add(mut self, rhs: Self) -> Self::Output {
         unsafe {
             for i in 0..self.state.len() {
                 for j in 0..self.state[i].len() {
-                    self.state[i][j] = vaddq_u32(self.state[i][j], rhs.state[i][j]);
+                    self.state[i][j].u32x4 =
+                        vaddq_u32(self.state[i][j].u32x4, rhs.state[i][j].u32x4);
                 }
             }
             self
@@ -33,10 +40,13 @@ macro_rules! rotate_left_epi32 {
 }
 
 impl Matrix {
-    #[inline(always)]
+    #[inline]
     fn quarter_round(&mut self) {
         unsafe {
-            for [a, b, c, d] in self.state.iter_mut() {
+            for [a, b, c, d] in self.state.iter_mut().map(|v| {
+                let u32x4_4: &mut [uint32x4_t; WIDTH] = transmute(v);
+                u32x4_4
+            }) {
                 *a = vaddq_u32(*a, *b);
                 *d = veorq_u32(*d, *a);
                 *d = rotate_left_epi32!(*d, 16);
@@ -56,10 +66,13 @@ impl Matrix {
         }
     }
 
-    #[inline(always)]
+    #[inline]
     fn make_diagonal(&mut self) {
         unsafe {
-            for [a, _, c, d] in self.state.iter_mut() {
+            for [a, _, c, d] in self.state.iter_mut().map(|v| {
+                let u32x4_4: &mut [uint32x4_t; WIDTH] = transmute(v);
+                u32x4_4
+            }) {
                 *a = vextq_u32(*a, *a, 3);
                 *c = vextq_u32(*c, *c, 1);
                 *d = vextq_u32(*d, *d, 2);
@@ -67,10 +80,13 @@ impl Matrix {
         }
     }
 
-    #[inline(always)]
+    #[inline]
     fn unmake_diagonal(&mut self) {
         unsafe {
-            for [a, _, c, d] in self.state.iter_mut() {
+            for [a, _, c, d] in self.state.iter_mut().map(|v| {
+                let u32x4_4: &mut [uint32x4_t; WIDTH] = transmute(v);
+                u32x4_4
+            }) {
                 *c = vextq_u32(*c, *c, 3);
                 *d = vextq_u32(*d, *d, 2);
                 *a = vextq_u32(*a, *a, 1);
@@ -80,8 +96,8 @@ impl Matrix {
 }
 
 impl Machine for Matrix {
-    #[inline(always)]
-    fn new(state: &ChaCha<Self>) -> Self {
+    #[inline]
+    fn new_djb(state: &ChaChaSmall) -> Self {
         unsafe {
             let mut result = Matrix {
                 state: [[
@@ -91,24 +107,50 @@ impl Machine for Matrix {
                     transmute(state.row_d),
                 ]; DEPTH],
             };
-            // Look what they need to mimic a fraction of my power.
-            result.state[1][3] = vaddq_u32(
-                result.state[1][3],
-                vreinterpretq_u32_u64(vcombine_u64(vcreate_u64(1), vcreate_u64(0))),
+            result.state[1][3].u64x2 = vaddq_u64(
+                result.state[1][3].u64x2,
+                vcombine_u64(vcreate_u64(1), vcreate_u64(0)),
             );
-            result.state[2][3] = vaddq_u32(
-                result.state[2][3],
-                vreinterpretq_u32_u64(vcombine_u64(vcreate_u64(2), vcreate_u64(0))),
+            result.state[2][3].u64x2 = vaddq_u64(
+                result.state[2][3].u64x2,
+                vcombine_u64(vcreate_u64(2), vcreate_u64(0)),
             );
-            result.state[3][3] = vaddq_u32(
-                result.state[3][3],
-                vreinterpretq_u32_u64(vcombine_u64(vcreate_u64(3), vcreate_u64(0))),
+            result.state[3][3].u64x2 = vaddq_u64(
+                result.state[3][3].u64x2,
+                vcombine_u64(vcreate_u64(3), vcreate_u64(0)),
             );
             result
         }
     }
 
-    #[inline(always)]
+    #[inline]
+    fn new_ietf(state: &ChaChaSmall) -> Self {
+        unsafe {
+            let mut result = Matrix {
+                state: [[
+                    transmute(ROW_A),
+                    transmute(state.row_b),
+                    transmute(state.row_c),
+                    transmute(state.row_d),
+                ]; DEPTH],
+            };
+            result.state[1][3].u32x4 = vaddq_u32(
+                result.state[1][3].u32x4,
+                vcombine_u32(vcreate_u32(1), vcreate_u32(0)),
+            );
+            result.state[2][3].u32x4 = vaddq_u32(
+                result.state[2][3].u32x4,
+                vcombine_u32(vcreate_u32(2), vcreate_u32(0)),
+            );
+            result.state[3][3].u32x4 = vaddq_u32(
+                result.state[3][3].u32x4,
+                vcombine_u32(vcreate_u32(3), vcreate_u32(0)),
+            );
+            result
+        }
+    }
+
+    #[inline]
     fn double_round(&mut self) {
         // Column rounds
         self.quarter_round();
@@ -118,10 +160,32 @@ impl Machine for Matrix {
         self.unmake_diagonal();
     }
 
-    #[inline(always)]
-    fn fill_block(self, buf: &mut [u64; BUF_LEN]) {
+    #[inline]
+    fn fill_block(self, buf: &mut [u8; BUF_LEN]) {
         unsafe {
             *buf = transmute(self);
+        }
+    }
+
+    #[inline]
+    fn increment_djb(&mut self) {
+        unsafe {
+            let increment = vcombine_u64(vcreate_u64(DEPTH as u64), vcreate_u64(0));
+            self.state[0][3].u64x2 = vaddq_u64(self.state[0][3].u64x2, increment);
+            self.state[1][3].u64x2 = vaddq_u64(self.state[1][3].u64x2, increment);
+            self.state[2][3].u64x2 = vaddq_u64(self.state[2][3].u64x2, increment);
+            self.state[3][3].u64x2 = vaddq_u64(self.state[3][3].u64x2, increment);
+        }
+    }
+
+    #[inline]
+    fn increment_ietf(&mut self) {
+        unsafe {
+            let increment = vcombine_u32(vcreate_u32(DEPTH as u64), vcreate_u32(0));
+            self.state[0][3].u32x4 = vaddq_u32(self.state[0][3].u32x4, increment);
+            self.state[1][3].u32x4 = vaddq_u32(self.state[1][3].u32x4, increment);
+            self.state[2][3].u32x4 = vaddq_u32(self.state[2][3].u32x4, increment);
+            self.state[3][3].u32x4 = vaddq_u32(self.state[3][3].u32x4, increment);
         }
     }
 }
