8c03af5392defecd65935cec71a99e3e40a93690
silverstillisntgold <nickdilla13@gmail.com>
2025-10-03 04:55:53 -0400

    more work on updated  abstraction layer

--- src/backends/mod.rs
+++ src/backends/mod.rs
@@ -7,9 +7,10 @@ mod avx512;
 mod soft;
 mod sse2;
 
-use crate::util::ChaChaNaked;
+use crate::{rounds::*, util::ChaChaNaked, variations::*};
 use core::{
     marker::PhantomData,
+    mem::transmute,
     ops::{Add, BitOr, BitXor, Deref, DerefMut},
 };
 
@@ -30,6 +31,11 @@ impl<T> Vector<T> {
     pub fn reverse(&mut self) {
         self.inner.reverse();
     }
+
+    #[inline(always)]
+    pub fn increment_idx<const IDX: usize>(&mut self) {
+        todo!()
+    }
 }
 
 macro_rules! rotate_left_epi32 {
@@ -42,12 +48,24 @@ macro_rules! rotate_left_epi32 {
     }};
 }
 
-pub trait VectorOps: Add + BitOr + BitXor + Sized {
+pub trait VectorOps:
+    Add<Output = Self> + BitOr<Output = Self> + BitXor<Output = Self> + Sized
+{
+    fn broadcast(value: i32) -> Self {
+        unimplemented!()
+    }
+
+    /// Not to be used directly.
+    ///
+    /// Shifts each internal `i32` by `IMM8` places to the left.
     fn shift_left<const IMM8: i64>(self) -> Self;
 
+    /// Not to be used directly.
+    ///
+    /// Shifts each internal `i32` by `IMM8` places to the right.
     fn shift_right<const IMM8: i64>(self) -> Self;
 
-    /// Shuffles the four internal 128-bit lanes using `IMM8` as a mask.
+    /// Shuffles the four internal 128-bit lanes using `IMM8` as a destination mask.
     fn shuffle_128<const IMM8: i32>(self) -> Self;
 }
 
@@ -59,9 +77,56 @@ pub struct Machine<T> {
     row_d: Vector<T>,
 }
 
+impl<T> From<[u8; 256]> for Machine<T> {
+    #[inline(always)]
+    fn from(value: [u8; 256]) -> Self {
+        unsafe { transmute(value) }
+    }
+}
+
+impl<T> From<Machine<T>> for [u8; 256] {
+    #[inline(always)]
+    fn from(value: Machine<T>) -> Self {
+        unsafe { transmute(value) }
+    }
+}
+
 impl<T> Machine<T>
 where
     T: VectorType,
     Vector<T>: VectorOps,
 {
+    #[inline(always)]
+    pub fn new<V: Variant>(state: &ChaChaNaked) -> Self {
+        todo!()
+    }
+
+    #[inline(always)]
+    pub fn increment<V: Variant>(&mut self) {
+        todo!()
+    }
+
+    #[inline(always)]
+    pub fn double_round(&mut self) {
+        todo!()
+    }
+
+    #[inline(always)]
+    pub fn get_inner(self) -> [u8; 256] {
+        // TODO: Data probably needs to be rearranged before being returned.
+        unsafe { transmute(self) }
+    }
+
+    #[inline(always)]
+    pub fn xor_inner(self, other: [u8; 256]) -> [u8; 256] {
+        // TODO: Same issue as in `Self::get_inner`.
+        let other = Self::from(other);
+        Self {
+            row_a: self.row_a ^ other.row_a,
+            row_b: self.row_b ^ other.row_b,
+            row_c: self.row_c ^ other.row_c,
+            row_d: self.row_d ^ other.row_d,
+        }
+        .into()
+    }
 }
