16893be2d9d81d86f07465f8048cc2e26c928e7b
silverstillisntgold <nickdilla13@gmail.com>
2026-08-18 02:24:02 -0400

    Update compare.rs

--- examples/compare.rs
+++ examples/compare.rs
@@ -16,32 +16,32 @@ fn main() {
         c1.fill(&mut buffer);
         let delta = start.elapsed().as_secs_f64();
         println!("Current implementation:");
-        println!("  time: {:.4}", delta);
-        println!("  GB/s: {:.4}", SIZE as f64 / delta / 1e9);
+        println!("  time: {:.3} seconds", delta);
+        println!("  GB/s: {:.3}", SIZE as f64 / delta / 1e9);
 
         let mut c2 = chachacha_042::ChaCha8Djb::from(buf);
         let start = Instant::now();
         c2.fill(&mut buffer);
         let delta = start.elapsed().as_secs_f64();
         println!("Previous implementation:");
-        println!("  time: {:.4}", delta);
-        println!("  GB/s: {:.4}", SIZE as f64 / delta / 1e9);
+        println!("  time: {:.3} seconds", delta);
+        println!("  GB/s: {:.3}", SIZE as f64 / delta / 1e9);
 
         let mut c3 = chacha20::ChaCha8Rng::from_rng(&mut rand::rng());
         let start = Instant::now();
         c3.fill_bytes(&mut buffer);
         let delta = start.elapsed().as_secs_f64();
         println!("ChaCha20 library:");
-        println!("  time: {:.4}", delta);
-        println!("  GB/s: {:.4}", SIZE as f64 / delta / 1e9);
+        println!("  time: {:.3} seconds", delta);
+        println!("  GB/s: {:.3}", SIZE as f64 / delta / 1e9);
 
         let mut c4 = rand_chacha::ChaCha8Rng::from_rng(&mut rand::rng());
         let start = Instant::now();
         c4.fill_bytes(&mut buffer);
         let delta = start.elapsed().as_secs_f64();
         println!("RandChaCha library:");
-        println!("  time: {:.4}", delta);
-        println!("  GB/s: {:.4}", SIZE as f64 / delta / 1e9);
+        println!("  time: {:.3} seconds", delta);
+        println!("  GB/s: {:.3}", SIZE as f64 / delta / 1e9);
         println!();
     }
 }
