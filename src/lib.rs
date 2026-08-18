4ce9bfb1a132608be729644b56a0af0906f4ab89
silverstillisntgold <nickdilla13@gmail.com>
2026-08-18 03:09:34 -0400

    version bump: 0.5.2

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,10 @@
 # ChaChaCha: ChaCha with a little extra Cha
 
 Extremely fast ChaCha implementation. Primarily made for use as a CRNG in the [`ya-rand`] crate,
-but should be just as usable anywhere else you might want to use ChaCha.
-
-## Examples
-
-```ignore
-use chachacha::{BUF_LEN_U64, BUF_LEN_U8, ChaCha12Djb};
-
-// Create a new `ChaCha12Djb` instance with a key that is all ones,
-// a counter starting at 69, and a nonce of 0 and 1 (the last nonce
-// value is discarded in the `Djb` variants).
-let mut chacha = ChaCha12Djb::new([u32::MAX; 8],
-                                   69,
-                                  [0, 1, 2]);
-// 256 bytes of output
-let block_output: [u8; BUF_LEN_U8] = chacha.get_block();
-let all_zeros = block_output.into_iter().all(|v| v == 0);
-assert!(!all_zeros);
-```
+but should be just as usable anywhere else you might want to use ChaCha. This is intended to be
+a low-level primitive, and should generally not be used directly. When used as a cipher it should
+be paired with something like Poly1305, and when used as an RNG it's output should be consumed
+and fed to the user batches.
 
 [`ya-rand`]: https://crates.io/crates/ya-rand
 */
